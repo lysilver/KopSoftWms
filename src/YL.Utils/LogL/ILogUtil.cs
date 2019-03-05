bdc604718db825648788f70dda697fe343739fa3
DiMing <2320690398@qq.com>
2019-03-05 19:31:36 +0800

    LogL/ILogUtil.cs

---
+++ src/YL.Utils/LogL/ILogUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace YL.Utils.Log
+{
+    public interface ILogUtil
+    {
+        void Debug(string msg);
+
+        void Debug(Exception exception);
+
+        void Debug(Exception exception, string msg);
+
+        void Info(string msg);
+
+        void Error(string msg);
+
+        void Fatal(string msg);
+
+        void Trace(string msg);
+    }
+}
---
+++ src/YL.Utils/LogL/NlogUtil.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+
+namespace YL.Utils.Log
+{
+    public class NlogUtil : ILogUtil
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public void Debug(string msg)
+        {
+            _logger.Debug(msg);
+        }
+
+        public void Debug(Exception exception, string msg)
+        {
+            _logger.Debug(exception, msg);
+        }
+
+        public void Debug(Exception exception)
+        {
+            _logger.Debug(exception);
+        }
+
+        public void Error(string msg)
+        {
+            _logger.Error(msg);
+        }
+
+        public void Fatal(string msg)
+        {
+            _logger.Fatal(msg);
+        }
+
+        public void Info(string msg)
+        {
+            _logger.Info(msg);
+        }
+
+        public void Trace(string msg)
+        {
+            _logger.Trace(msg);
+        }
+    }
+}
