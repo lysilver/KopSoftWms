ae19ae1219625d5f6225fa76e05a7f5d850a23a4
DiMing <2320690398@qq.com>
2020-01-01 21:47:51 +0800

    CheckNull

--- src/YL.Utils/Check/CheckNull.cs
+++ src/YL.Utils/Check/CheckNull.cs
@@ -9,23 +9,23 @@ namespace YL.Utils.Check
             where TArgument : class
         {
             if (argument == null)
-                throw new ArgumentNullException(nameof(argumentName));
+                throw new ArgumentNullException(argumentName);
         }
 
         public static void ArgumentIsNullException(string argument, string argumentName = "不能为空")
         {
             if (argument.IsNull2())
-                throw new ArgumentException(nameof(argumentName));
+                throw new ArgumentException(argumentName);
         }
 
         public static void ArgumentIsNullException(string argumentName = "不能为空")
         {
-            throw new ArgumentException(nameof(argumentName));
+            throw new ArgumentException(argumentName);
         }
 
         public static void ThrowException(string argumentName = "未实现")
         {
-            throw new Exception(nameof(argumentName));
+            throw new Exception(argumentName);
         }
     }
 }
