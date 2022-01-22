2a703010522f718d386131abc494bcaba6793d71
Matthew Elin <m_elin@outlook.com>
2022-01-22 06:42:50 +0500

    Add globalization support on startup

--- src/Entity/Fluent.Validation/CarrierFluent.cs
+++ src/Entity/Fluent.Validation/CarrierFluent.cs
@@ -6,7 +6,7 @@ namespace YL.Core.Entity.Fluent.Validation
     {
         public CarrierFluent()
         {
-            RuleFor(x => x.CarrierNo).NotNull().NotEmpty().WithMessage("承运商编号不能为空").Length(1, 20).WithMessage("承运商编号长度不能超过20");
+            RuleFor(x => x.CarrierNo).NotNull().NotEmpty().WithMessage(Properties.Resources.OPERATION_NOTNULL).Length(1, 20).WithMessage("承运商编号长度不能超过20");
             RuleFor(x => x.CarrierName).NotNull().NotEmpty().WithMessage("承运商名称不能为空").Length(1, 50).WithMessage("承运商编号长度不能超过50");
             RuleFor(x => x.Remark).MaximumLength(200).WithMessage("备注长度不能超过200");
         }
